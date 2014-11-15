a628a4ec736096b44f0fe47cfafc8ba77777d34f
jitendra singh <jitendra.nov27@gmail.com>
2014-11-15 17:56:25 +0530

    changed main file

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world! with git, nov 15')
+        self.response.write('Hello world! with git, nov')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
